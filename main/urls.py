19136d25e04ad6024c392498eec0726f6ff26efa
cahyacoding <cahya.bgg@gmail.com>
2024-08-15 12:15:57 +0700

    Tutorial 2 selesai

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from main.views import show_main
+from main.views import show_main, create_book
 
 
-# app_name = 'main'
+app_name = 'main'
 
-# urlpatterns = [
-#     path('', show_main, name='show_main'),
-# ]
+urlpatterns = [
+    path('', show_main, name='show_main'),
+    path('create-book', create_book, name='create_book'),
+]
--- main/views.py
+++ main/views.py
@@ -1,9 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from main.forms import BookForm
+from main.models import Book
 
 def show_main(request):
+    books = Book.objects.all()
+
     context = {
-        'name': 'Cahya Bagus Gautama Gozales',
-        'class': 'PBP E'
+        'name': 'Pak Bepe',
+        'class': 'PBP A',
+        'books': books
     }
 
     return render(request, "main.html", context)
+
+def create_book(request):
+    form = BookForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return redirect('main:show_main')
+
+    context = {'form': form}
+    return render(request, "create_book.html", context)
