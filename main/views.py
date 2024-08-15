82378913d42134a98a9da5eb02f8826e8c75f9ac
cahyacoding <cahya.bgg@gmail.com>
2024-08-15 12:16:14 +0700

    Tutorial 2 selesai

--- main/views.py
+++ main/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect
 from main.forms import BookForm
 from main.models import Book
+from django.http import HttpResponse
+from django.core import serializers
 
 def show_main(request):
     books = Book.objects.all()
@@ -13,6 +15,7 @@ def show_main(request):
 
     return render(request, "main.html", context)
 
+
 def create_book(request):
     form = BookForm(request.POST or None)
 
@@ -22,3 +25,19 @@ def create_book(request):
 
     context = {'form': form}
     return render(request, "create_book.html", context)
+
+def show_xml(request):
+    data = Book.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = Book.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request,id):
+    data = Book.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json_by_id(request,id):
+    data = Book.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json",data),content_type= "application/json")
