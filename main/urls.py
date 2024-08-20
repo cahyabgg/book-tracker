b6ec5f978f1b3b32d3cc452ff5f80f7163dc1331
cahyacoding <cahya.bgg@gmail.com>
2024-08-20 21:22:25 +0700

    tutorial 4 selesai

--- main/urls.py
+++ main/urls.py
@@ -5,7 +5,8 @@ from main.views import (
     show_main,create_book, 
     show_xml,show_json, 
     show_xml_by_id,show_json_by_id, 
-    register,login_user,logout_user
+    register,login_user,logout_user,
+    show_css,edit_book,delete_book
 )
 
 app_name = 'main'
@@ -20,4 +21,6 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
+    path('edit-book/<int:id>', edit_book, name='edit_book'),
+    path('delete/<int:id>', delete_book, name='delete_book'), 
 ]
